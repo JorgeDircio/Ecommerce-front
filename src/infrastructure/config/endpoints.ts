920e8c7a02404ebfc7bacf57698f70af18f3274f
JorgeDircio <jorge.carreon@kodevox.com>
2025-03-31 14:09:15 -0600

    fix: se corrige construccion de la imagen

--- src/infrastructure/config/endpoints.ts
+++ src/infrastructure/config/endpoints.ts
@@ -1,5 +1,7 @@
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "";
 
+console.log("🔗 URL del backend:", process.env.NEXT_PUBLIC_API_URL);
+
 export const Endpoints = {
 	checkout: `${BASE_URL}/transactions`,
 	getTransactionById: (id: string) => `${BASE_URL}/transactions/${id}`,
